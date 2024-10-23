bf8bf9e6eea45b0f75af72c5591e07798fed794d
Artur Trębski <137427463+SelfishCrayfish@users.noreply.github.com>
2024-10-23 16:01:34 +0200

    Delete app directory

--- app/src/main/java/Utils.java
+++
@@ -1,8 +0,0 @@
-import android.text.TextUtils;
-import android.util.Patterns;
-
-public class Utils {
-    public static boolean isValidEmail(CharSequence target) {
-        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
-    }
-}
