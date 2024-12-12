5bac73b4e1f031822d9109f75f6ce940e3474b02
Artur Trębski <trebski.artur@gmail.com>
2024-12-12 17:18:19 +0100

    Changing language

--- app/src/main/java/com/example/commbidapp/UserProfileActivity.kt
+++ app/src/main/java/com/example/commbidapp/UserProfileActivity.kt
@@ -1,20 +1,15 @@
 package com.example.commbidapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.commbidapp.ui.theme.CommissionScreen
-import com.example.commbidapp.ui.theme.MainScreenWithNavbar
 import com.example.commbidapp.ui.theme.UserProfileScreen
 
 class UserProfileActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreenWithNavbar {
-                UserProfileScreen()
-            }
+            UserProfileScreen()
         }
     }
 }
